fe6e65c0bec7617b2626c65da99d1c65485da564
JeremieSaubi <jsaubi@gmail.com>
2017-11-21 00:06:21 +0100

    maj

--- page-templates/page-woo.php
+++ page-templates/page-woo.php
@@ -1,25 +1,27 @@
 <?php
 /*
-Template Name: Modele page test
+Template Name: Modele page WOO
  */
 ?>
 
-<?php get_header(); ?>
+    <?php get_header(); ?>
 
-<section id="cj_template">
+    <section id="woocommerce">
 
-<article class="cj_article-template">
+        <article class="container">
 
-   <?php while (have_posts()) : the_post(); ?>
-<h1><?php the_title(); ?></h1>
-    <hr />
-<?php the_content(); ?>
-<?php endwhile; ?>
-
-
-</article>
-
-</section>
+            <?php while (have_posts()) : the_post(); ?>
+            <h1>
+                <?php the_title(); ?>
+            </h1>
+            <hr />
+            <?php the_content(); ?>
+            <?php endwhile; ?>
 
 
-<?php get_footer(); ?>
+        </article>
+
+    </section>
+
+
+    <?php get_footer(); ?>
