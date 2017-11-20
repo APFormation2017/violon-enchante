c9b7056a55567a5a14d5786e5d1872483c1280ef
JeremieSaubi <jsaubi@gmail.com>
2017-11-20 17:07:39 +0100

    maj

--- woocommerce.php
+++ woocommerce.php
@@ -3,6 +3,13 @@
 <section id="woocommerce">
 
     <article class="container">
+<!--     MENU BOUTIQUE -->
+      <div id="menu-boutique">
+                   <?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_class' => 'flex', 'container' => 'nav'
+										   ) ); ?>
+				</div>
+
+<!--       MISE EN PLACE DU FIL D'ARIANE -->
         <?php
         if ( function_exists('yoast_breadcrumb') ) {
             yoast_breadcrumb('
@@ -10,6 +17,8 @@
             ');
         }
         ?>
+
+<!--        AFFICHAGE BOUTIQUE-->
         <?php woocommerce_content(); ?>
 
 
